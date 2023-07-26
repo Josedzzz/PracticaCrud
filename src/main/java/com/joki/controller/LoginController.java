1981cf548ec4744ff16572c804d218d11bf6b633
jokizz <josedavidamayar@gmail.com>
2023-07-26 00:03:01 -0500

    Interfaz login

--- src/main/java/com/joki/controller/LoginController.java
+++ src/main/java/com/joki/controller/LoginController.java
@@ -6,13 +6,24 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
     @FXML
-    private Label lblPrueba;
+    private Button btnLogin;
+
+    @FXML
+    private AnchorPane txtContrasenia;
+
+    @FXML
+    private TextField txtUsuario;
 
     private Application application;
     private Stage stage;
@@ -30,4 +41,9 @@ public class LoginController implements Initializable {
         this.stage = stage;
     }
 
+    //-------------------------- FUNCIONES LOGIN ------------------------------------------------------------
+    @FXML
+    void loginEmpleado(ActionEvent event) {
+
+    }
 }
